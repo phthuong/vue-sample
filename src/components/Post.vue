576c2639f91f68691d25592831f2f558d10e40ac
Huong Pham <phthuong@tma.com.vn>
2018-09-14 18:13:38 +0700

    New Vue Sample Project

---
+++ src/components/Post.vue
@@ -0,0 +1,10 @@
+<script>
+const Post = {
+  template: '<div>Post: {{ $route.params.post_slug }}</div>',
+  beforeRouteUpdate (to, from, next) {
+    console.log(`Updating slug from ${from} to ${to}`)
+    next() // make sure you always call next()
+  }
+}
+export default Post
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+import Post from '@/components/Post'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    { path: '/', name: 'Home', component: Home },
+    { path: '/post/:post_slug', name: 'Post', component: Post }
+  ]
+})
